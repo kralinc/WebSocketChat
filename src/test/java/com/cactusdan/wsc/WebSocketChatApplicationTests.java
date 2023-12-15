82add33a2f70ee3349a16bdac4b7adbf89a0cb37
kralinc <lelinxi@protonmail.com>
2023-12-15 12:25:24 -0600

    initial commit

---
+++ src/test/java/com/cactusdan/wsc/WebSocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cactusdan.wsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
